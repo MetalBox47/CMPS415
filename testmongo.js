71a086597b6d4071e32531997ce2ca3703223d2c
MetalBox47 <35822953+MetalBox47@users.noreply.github.com>
2024-03-04 20:21:56 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://caseylaiche:<password>@cluster0.bnthnra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://caseylaiche:Musicnote4$4$@cluster0.bnthnra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
