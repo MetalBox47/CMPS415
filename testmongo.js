6e50e7bad5cc59aed4728d76e1cc08f7554c75b4
MetalBox47 <35822953+MetalBox47@users.noreply.github.com>
2024-03-04 20:07:49 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -39,9 +39,9 @@ async function run() {
     const parts = database.collection('Samples');
 
     // Hardwired Query for a part that has partID '12345'
-    // const query = { partID: '12345' };
+    const query = { partID: '12345' };
     // But we will use the parameter provided with the route
-    const query = { partID: req.params.item };
+    // const query = { partID: req.params.item };
 
     const part = await parts.findOne(query);
     console.log(part);
